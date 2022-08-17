44ed85881a50d70b7dfd1ca3d431d1763d4aa10b
seokyHan <sseok1979@naver.com>
2022-08-17 09:51:39 +0900

    remove git files

---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="side-bar-wrapper">
+    <VueResizable
+        class="resizable-side-bar"
+        :width="500"
+        :min-width="500"
+        :max-width="Infinity"
+        :active="['r']"
+    >
+      <div class="side-bar"></div>
+    </VueResizable>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style scoped lang="scss">
+  .side-bar-wrapper {
+  > .side-bar {
+    background-color: #000000;
+    opacity: 0.5;
+    width: 500px;
+    height: 100%;
+  }
+}
+</style>
